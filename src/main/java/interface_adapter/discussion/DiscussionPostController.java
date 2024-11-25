e56fca524631d1e43c3d19e05138c3d77fccb817
raghavatruntime <astroraghav04@gmail.com>
2024-11-25 15:54:43 -0500

    Implemented DiscussionPostController

--- src/main/java/interface_adapter/discussion/DiscussionPostController.java
+++ src/main/java/interface_adapter/discussion/DiscussionPostController.java
@@ -1,4 +1,21 @@
 package interface_adapter.discussion;
 
+import use_case.discussion.DiscussionPostInputBoundary;
+import use_case.discussion.DiscussionPostInputData;
+
 public class DiscussionPostController {
+    private final DiscussionPostInputBoundary interactor;
+
+    public DiscussionPostController(DiscussionPostInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void addPost(String userId, String content) {
+        DiscussionPostInputData inputData = new DiscussionPostInputData(userId, content);
+        interactor.addPost(inputData);
+    }
+
+    public void getAllPosts() {
+        interactor.getAllPosts();
+    }
 }
