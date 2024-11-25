3b69fb864f2f0b39ade29c41cd7f1a3569fac170
raghavatruntime <astroraghav04@gmail.com>
2024-11-25 15:54:53 -0500

    Implemented DiscussionPostPresenter

--- src/main/java/interface_adapter/discussion/DiscussionPostPresenter.java
+++ src/main/java/interface_adapter/discussion/DiscussionPostPresenter.java
@@ -1,4 +1,24 @@
 package interface_adapter.discussion;
 
-public class DiscussionPostPresenter {
+import entity.DiscussionPost;
+import use_case.discussion.DiscussionPostOutputBoundary;
+
+import java.util.List;
+
+public class DiscussionPostPresenter implements DiscussionPostOutputBoundary {
+    private final DiscussionPostViewModel viewModel;
+
+    public DiscussionPostPresenter(DiscussionPostViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void presentPostAdded() {
+        // Handle post added (if needed)
+    }
+
+    @Override
+    public void presentAllPosts(List<DiscussionPost> posts) {
+        viewModel.setPosts(posts);
+    }
 }
