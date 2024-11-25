ecdfc9b7e122c44b8ba1cfd823c4b1a2a129d96b
raghavatruntime <astroraghav04@gmail.com>
2024-11-25 15:55:07 -0500

    Implemented DiscussionPostState

--- src/main/java/interface_adapter/discussion/DiscussionPostState.java
+++ src/main/java/interface_adapter/discussion/DiscussionPostState.java
@@ -1,4 +1,27 @@
 package interface_adapter.discussion;
 
 public class DiscussionPostState {
+    private String userId;
+    private String content;
+
+    public DiscussionPostState(String userId, String content) {
+        this.userId = userId;
+        this.content = content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
