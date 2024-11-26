71215a5c82b075a7525a9a315480ae7e224c3314
raghavatruntime <astroraghav04@gmail.com>
2024-11-25 23:26:51 -0500

    Refactored to include constructor and getters/setters.

--- src/main/java/entity/GameState.java
+++ src/main/java/entity/GameState.java
@@ -3,11 +3,28 @@ package entity;
 import interface_adapter.grid.GridState;
 
 public class GameState {
-    private int level;
+    private final int level;
     private int remainingAttempts;
-    private GridState gridState;
+    private final GridState gridState;
+
+    public GameState(int level, GridState gridState) {
+        this.level = level;
+        this.gridState = gridState;
+    }
 
     // Getters and setters
+    public void setRemainingAttempts(int remainingAttempts) {
+        this.remainingAttempts = remainingAttempts;
+    }
+    public int getRemainingAttempts() {
+        return remainingAttempts;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public GridState getGridState() {
+        return gridState;
+    }
 
     public GuessResult checkGuess(String guess) {
         // Delegate the guess checking to GridState
