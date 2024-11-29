6809ea0c7bf4cf21d2a74c33c741745cd5431203
raghavatruntime <astroraghav04@gmail.com>
2024-11-28 21:24:22 -0500

    Added fetchRandomQuote method to fetch quote from API

--- src/main/java/use_case/discussion/DiscussionPostInputBoundary.java
+++ src/main/java/use_case/discussion/DiscussionPostInputBoundary.java
@@ -3,6 +3,6 @@ package use_case.discussion;
 public interface DiscussionPostInputBoundary {
     void addPost(DiscussionPostInputData inputData);
     void getAllPosts();
-
+    void fetchRandomQuote(String userId);
     void switchToInstructionView();
 }
