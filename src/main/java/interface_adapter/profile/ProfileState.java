2313899edfea8c529fceede53d8b35385cda1068
Geon Lim <geon.lim@mail.utoronto.ca>
2024-11-30 17:39:20 -0500

    change some codes for game end view

--- src/main/java/interface_adapter/profile/ProfileState.java
+++ src/main/java/interface_adapter/profile/ProfileState.java
@@ -1,18 +1,9 @@
 package interface_adapter.profile;
 
 public class ProfileState {
-    private int win;
     private String username = "";
     private String loginError;
     private String password = "";
-
-    public int getWin() {
-        return win;
-    }
-
-    public void setWin(int win) {
-        this.win = win;
-    }
 
     public String getUsername() {
         return username;
